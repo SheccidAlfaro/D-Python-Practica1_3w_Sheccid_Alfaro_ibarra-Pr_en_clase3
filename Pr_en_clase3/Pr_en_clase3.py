b1852d9b4b17c2aad422bc5efd46d20998b8a917
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-09-25 20:13:59 -0600

    Add files via upload

---
+++ Pr_en_clase3/Pr_en_clase3.py
@@ -0,0 +1,10 @@
+print("")
+print("Convertir grados farenheit a celcius Alfaro Ibarra Sheccid 3W.")
+#Declarar varible y preguntar al usuario 
+gf=float(input("ingrese los grados farenheit que desee convertir a celcius:"))
+#Declarar variable para colocar la formula
+Celsius=5/9*gf-32
+#Se,muestra el resultado en pantalla (los grados celcius)
+print("Grados celcius")
+print(Celsius)
+print("")
